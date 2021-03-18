de01fc8700c1758b6c0114c16e449df98df50eff
tripathivivek1998 <tripathivivek1998@gmail.com>
2021-03-18 23:20:20 +0530

    Source code added Signed-off-by: tripathivivek1998 <tripathivivek1998@gmail.com>

---
+++ Source/src/javaconnect.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Royal Mann
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    Connection conn;
+
+    /**
+     *
+     * @return
+     */
+    public static Connection ConnecrDb()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/librarynew","root","");
+            return conn;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+}
